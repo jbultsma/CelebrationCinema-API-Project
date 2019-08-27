5b817bb38566878bb3524bc204f09baf55b12202
OppaPlzHajima <51772642+OppaPlzHajima@users.noreply.github.com>
2019-08-26 21:54:37 -0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GRVL
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public string Status;
+        public DateTime DueDate;
+
+        public Book(string title, string author, string status)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Status = status;
+        }
+        public Book(string title, string author, string status, DateTime dueDate)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Status = status;
+            this.DueDate = dueDate;
+        }
+    }
+}
