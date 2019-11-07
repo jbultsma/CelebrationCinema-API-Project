40d2853599be3b496c6cd06d7fb750e7d8ab115b
OppaPlzHajima <51772642+OppaPlzHajima@users.noreply.github.com>
2019-11-06 22:59:03 -0500

    updating changes to model, controller

---
+++ OneDrive/Desktop/Celebration Cinema/CelebrationCinema-API-Project/CelebrationCinema/Models/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CelebrationCinema.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public DbSet<Movies> Movie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-2TSRG7K\SQLEXPRESS01;Trusted_Connection=True;Initial Catalog=MoviesDb;");
+        }
+    }
+
+}
