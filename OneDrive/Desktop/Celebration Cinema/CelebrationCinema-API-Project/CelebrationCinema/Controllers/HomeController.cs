9adcb76cd2a54a6d7c91de465e124ed16cfcca44
OppaPlzHajima <51772642+OppaPlzHajima@users.noreply.github.com>
2019-11-06 21:29:01 -0500

    added model and controller api connection

--- OneDrive/Desktop/Celebration Cinema/CelebrationCinema-API-Project/CelebrationCinema/Controllers/HomeController.cs
+++ OneDrive/Desktop/Celebration Cinema/CelebrationCinema-API-Project/CelebrationCinema/Controllers/HomeController.cs
@@ -5,11 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CelebrationCinema.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace CelebrationCinema.Controllers
 {
     public class HomeController : Controller
     {
+        public string CallMovieAPI()
+        {
+            
+
+                HttpWebRequest request = WebRequest.CreateHttp($"http://www.omdbapi.com/?i=tt3896198&apikey=c794a6f9");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                //This will convert the response to a string
+                StreamReader rd = new StreamReader(response.GetResponseStream());
+
+                string APIText = rd.ReadToEnd();
+
+                return APIText;
+         }
+
+        public JToken ParseJsonString(string text)
+        {
+            JToken output = JToken.Parse(text);
+            return output;
+        }
+
         public IActionResult Index()
         {
             return View();
