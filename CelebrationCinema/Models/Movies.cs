7da2bda3e74c3441d0a5a8bcdb54e6f53844b35c
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-11-10 21:09:22 -0500

    added Jtocken.
    this was already in the master but I changed some of the numbers to match the API, like the ID should say imdbID instead

--- CelebrationCinema/Models/Movies.cs
+++ CelebrationCinema/Models/Movies.cs
@@ -1,18 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
 
-namespace CelebrationCinema.Models
+
+
+namespace CelebtationCinema.Models
 {
     public class Movies
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int Year { get; set; }
-        public string Rated { get; set; }
-        public string Genre { get; set; }
-        public string Director { get; set; }
-        public string Poster { get; set; }
+            [Key]
+            public string ImdbId { get; set; }
+            public string Title { get; set; }
+            public int Year { get; set; }
+            public string Rated { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Poster { get; set; }
+
+
+            public Movies()
+            {
+
+            }
+
+            public Movies(JToken u)
+            {
+                this.ImdbId = u["imdbID"].ToString();
+                this.Title = u["Title"].ToString();
+                this.Year = int.Parse(u["Year"].ToString());
+                this.Rated = u["Rated"].ToString();
+                this.Genre = u["Genre"].ToString();
+                this.Director = u["Director"].ToString();
+                this.Poster = u["Poster"].ToString();
+            }
+        }
     }
-}
+
+
